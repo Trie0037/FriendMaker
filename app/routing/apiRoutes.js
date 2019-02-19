05fd92d1e803378feb52dd588f6dcda04989aa4e
Trie0037 <trie0037@gmail.com>
2019-02-18 22:23:18 -0600

    modal1

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,37 +1,34 @@
-// Your apiRoutes.js file should contain two routes:
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 var friends = require("../data/friends");
 
-
 module.exports = function(app) {
-	
-   app.get("/api/friends", function(req,res) {
-	res.json(friends);
-   });
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
-   
+  app.post("/api/friends", function(req, res) {
+    var userInput = req.body;
 
-   app.post("/api/friends", function(req,res) {
-    
-   var newFriend = req.body;
-   console.log(req.body);
-   res.json(req.body)
-   });
+    var userResponses = userInput.scores;
 
-    // app.post("/api/surveySubmit", function(req,res) {
-    // friends.push(req.body);
-   	// console.log(req.body);
+    var matchName = "";
+    var matchImage = "";
+    var totalDifference = 10000;
 
-    // var newFriend = req.body;
-    // var friendsArray =[];
+    for (var i = 0; i < friends.length; i++) {
+      var diff = 0;
+      for (var j = 0; j < userResponses.length; j++) {
+        diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+      }
 
-    //     for (var i=0; i<friendsArray; i++){
-    //   var scoresDiff=0;
+      if (diff < totalDifference) {
+        totalDifference = diff;
+        matchName = friends[i].name;
+        matchImage = friends[i].photo;
+      }
+    }
 
-    //   for (var j=0; j<newFriend.length; j++){
-    //      scoresDiff += (Math.abs(parseInt(friends[i].scores[j])-parseInt(newFriend[j])));
-    //   }
-    //   newFriend.push(scoresDiff);
-    //  }
-   }
+    friends.push(req.body);
+
+    res.json({ status: "OK", matchName: matchName, matchImage: matchImage });
+  });
+};
